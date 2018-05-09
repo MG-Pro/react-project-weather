a1c9f6dd5af08a2942c611d51fee5eeabb792558
MG <mg84@bk.ru>
2018-05-09 23:57:52 +0300

    add bootstrap

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,55 @@
 import React, {Component} from 'react';
 import WeatherDisplay from './WeatherDisplay';
+import {Navbar, NavbarBrand, Container, Label, Input, Row, Col} from "reactstrap";
 import './App.css';
 
-const PLACES = [
-  {name: "Palo Alto", zip: "94303"},
-  {name: "San Jose", zip: "94088"},
-  {name: "Santa Cruz", zip: "95062"},
-  {name: "Honolulu", zip: "96803"}
-];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activePlace: 0
+      coords: {
+        lat: 0,
+        lon: 0
+      }
     }
+  }
 
+
+  componentWillMount() {
+    fetch('http://api.sypexgeo.net/json/').then(res => res.json()).then(json => {
+      this.setState({
+        coords: {
+          lat: json.city.lat,
+          lon: json.city.lon
+        }
+      });
+    });
   }
 
   render() {
     return (
       <div>
-        {PLACES.map((item, i) => {
-          return <button
-            key = {i}
-            onClick = {() => {
-              this.setState({
-                activePlace: i
-              })
-            }}
-          >
-            {item.name}
-            </button>
-        })}
-        <WeatherDisplay zip = {PLACES[this.state.activePlace].name}/>
+        <Navbar dark color = {'primary'}>
+          <Container>
+            <NavbarBrand href = "/">
+              React Simple Weather App
+            </NavbarBrand>
+          </Container>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col className='mt-3 mb-3'>
+              <Label for="city">City</Label>
+              <Input type="text" id="city" placeholder="Start input" />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm = {8} md = {{size: 6, offset: null}}>
+              <WeatherDisplay coords = {this.state.coords}/>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
